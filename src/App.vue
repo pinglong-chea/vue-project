bf46aaadb4b24b22fd6b8082328a12abfa00f842
Chea Pinglong <c.pinglong@dgc.gov.kh>
2025-08-18 17:14:53 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CoffeePlan from './components/CoffeePlan.vue'
+import { ref } from 'vue'
+const plans = ref(['The Single', 'The Curious', 'The Addict', 'The Hacker'])
+
+</script>
+
+<template>
+  <div class="content">
+    <h1 class="title">Coffee Plans</h1>
+
+    <h2 class="subtitle">
+      We travel the world to source the very best single origin coffee for you
+    </h2>
+
+    <div class="plans">
+      <CoffeePlan v-for="plan in plans" :key="plan" :name="plan"></CoffeePlan>
+    </div>
+  </div>
+</template>
