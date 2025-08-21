f57a2546b9835da24d26eb50967580393261bcb1
C-pinglong <c.pinglong@dgc.gov.kh>
2025-08-22 01:13:17 +0700

    lesson 5

--- src/components/CoffeePlan.vue
+++ src/components/CoffeePlan.vue
@@ -1,10 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+
 defineProps({
   name: { type: String, default: 'Default Plan' },
 })
+const emit = defineEmits(['hi-there'])
+
+const selected = ref(false)
+function selectPlan () {
+  selected.value = !selected.value
+  emit('hi-there')
+}
 </script>
 <template>
-  <div class="plan">
+  <div @click="selectPlan" class="plan" :class="{ 'active-plan': selected }">
     <div class="description">
       <span class="title">{{ name }}</span>
     </div>
--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,8 +1,7 @@
 <script setup>
-import { ref } from 'vue';
-import CoffeePlan from './CoffeePlan.vue';
+import { ref } from 'vue'
+import CoffeePlan from './CoffeePlan.vue'
 const plans = ref(['The Single', 'The Curious', 'The Addict', 'The Hacker'])
-
 </script>
 <template>
   <div class="plans">
