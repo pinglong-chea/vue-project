8354e70d4bf9e73a2ba92cc80be6d4703649b462
Chea Pinglong <c.pinglong@dgc.gov.kh>
2025-10-13 16:54:39 +0700

    feat: update github page and card ui

--- src/components/GithubCard.vue
+++ src/components/GithubCard.vue
@@ -1,23 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+
 const props = defineProps({
   username: { type: String, required: true },
+  alignLeft: { type: Boolean, default: true }
 })
 
-const user = ref()
+const user = ref(null)
 
-fetch(`https://api.github.com/users/${props.username}`).then(async (res) => {
-  const data = await res.json()
-  user.value = data
+onMounted(async () => {
+  const response = await fetch(`https://api.github.com/users/${props.username}`)
+  user.value = await response.json()
 })
 </script>
 
 <template>
-  <div v-if="user" class="card card-side shadow-sm">
-    <div class="card-body">
-      <h2 class="card-title">{{ user.name }}</h2>
-      <p><strong>Followers:</strong> {{ user.followers }}</p>
-      <p><strong>Followings:</strong> {{ user.following }}</p>
+  <div v-if="user" :class="[
+    props.alignLeft
+      ? 'justify-center md:justify-self-start'
+      : 'justify-center md:justify-self-end'
+  ]">
+    <div class="card bg-secondary/30 shadow-sm card-lg md:w-96">
+      <div class="card-body">
+        <h2 class="card-title">{{ user.name }}</h2>
+        <p><strong>Followers:</strong> {{ user.followers }}</p>
+        <p><strong>Following:</strong> {{ user.following }}</p>
+      </div>
     </div>
   </div>
 </template>
--- src/views/GithubPage.vue
+++ src/views/GithubPage.vue
@@ -1,12 +1,19 @@
 <script setup>
+import { BiHomeAlt } from "react-icons/bi";
+
+
 import GithubCard from '@/components/GithubCard.vue'
+const usernames = ['mojombo', 'defunkt', 'pjhyett', 'torvalds', 'gaearon']
 </script>
-
 <template>
-  <h1>Github Page</h1>
-  <GithubCard username="mojombo"></GithubCard>
-  <GithubCard username="defunkt"></GithubCard>
-  <GithubCard username="pjhyett"></GithubCard>
-
-  <router-link to="/">Back home</router-link>
+  <div class=" flex flex-col gap-y-6 md:gap-y-8">
+    <div class="badge badge-xl bg-primary/40 border-none self-end">
+      <h1>Github</h1>
+    </div>
+    <div class="grid gap-y-6 grid-cols-1">
+      <GithubCard v-for="(username, index) in usernames" :key="username" :username="username"
+        :align-left="index % 2 === 0" />
+    </div>
+    <router-link to="/" class="w-fit"><BiHomeAlt /> <button class="btn bg-success/50">Back home</button> </router-link>
+  </div>
 </template>
