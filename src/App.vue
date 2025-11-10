c634000ba714d12f7075bcfe7c6709242463f5ae
Chea Pinglong <c.pinglong@dgc.gov.kh>
2025-11-10 17:07:56 +0700

    feat: add image and data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<script setup></script>
-
 <template>
-  <div class="bg-gradient-to-l from-[#24243e] via-[#302b63] to-[#0f0c29] min-h-screen">
-    <div class="max-w-7xl mx-auto py-8 md:py-12 lg:px-[125px] xl:px-0 px-4">
-      <router-view />
-    </div>
+  <TheNavigation />
+  <div class="container">
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+import TheNavigation from '@/components/TheNavigation.vue';
+
+export default {
+  components: { TheNavigation }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
-import GithubPage from '@/views/GithubPage.vue'
-import PlanPickerPage from '@/views/PlanPickerPage.vue'
-import AlertPage from '@/views/AlertPage.vue'
+// import GithubPage from '@/views/GithubPage.vue'
+// import PlanPickerPage from '@/views/PlanPickerPage.vue'
+// import AlertPage from '@/views/AlertPage.vue'
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/github', component: GithubPage },
-  { path: '/plan', component: PlanPickerPage },
-  { path: '/alert', component: AlertPage },
+  { path: '/brazil', component: () => import('@/views/BrazilPage.vue') },
+  { path: '/hawaii', component: () => import('@/views/HawaiiPage.vue') },
+  { path: '/jamaica', component: () => import('@/views/JamaicaPage.vue') },
+  { path: '/panama', component: () => import('@/views/PanamaPage.vue') },
+  // { path: '/github', component: GithubPage },
+  // { path: '/plan', component: PlanPickerPage },
+  // { path: '/alert', component: AlertPage },
 ]
 
 const router = createRouter({
