613857345aaf74ad98e648263e766e417db2f5dc
Chea Pinglong <c.pinglong@dgc.gov.kh>
2025-11-19 17:20:13 +0700

    feat: add route transitions

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,22 +1,24 @@
 <template>
-  <section v-if="destination" class="destination">
-    <h1>{{ destination.name }}</h1>
-    <GoBack />
-    <div class="destination-details">
-      <img :src="`/images/${destination.image}`" :alt="destination.name">
-      <p>{{ destination.description }}</p>
-    </div>
-  </section>
-  <section class="experiences">
-    <h2>Top Experiences in {{ destination.name }}</h2>
-    <div class="cards">
-      <router-link v-for="experience in destination.experiences" :key="experience.slug"
-        :to="{ name: 'experience.show', params: { experienceSlug: experience.slug } }">
-        <ExperienceCard :experience="experience" />
-      </router-link>
-    </div>
-    <router-view />
-  </section>
+  <div>
+    <section v-if="destination" class="destination">
+      <h1>{{ destination.name }}</h1>
+      <GoBack />
+      <div class="destination-details">
+        <img :src="`/images/${destination.image}`" :alt="destination.name">
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+    <section class="experiences">
+      <h2>Top Experiences in {{ destination.name }}</h2>
+      <div class="cards">
+        <router-link v-for="experience in destination.experiences" :key="experience.slug"
+          :to="{ name: 'experience.show', params: { experienceSlug: experience.slug } }">
+          <ExperienceCard :experience="experience" />
+        </router-link>
+      </div>
+      <router-view />
+    </section>
+  </div>
 </template>
 
 <script>
